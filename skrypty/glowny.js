1a3b84c73c2613288240f17a0d2004d2d18a9e8e
Kosiarznerek <dbabka.poczta@gmail.com>
2019-04-20 20:44:30 +0200

    Graphics loading and background

--- skrypty/glowny.js
+++ skrypty/glowny.js
@@ -1,16 +1,41 @@
 // Importy
+import {ladujGrafike} from './ladujGrafike.js';
 import {Wektor} from './Wektor.js';
 import {Prostokat} from './Prostokat.js';
 
-// Po załadowaniu strony
-window.addEventListener('load', setup);
+/**
+ * Grafiki do gry
+ * @type {{flappyBird: Image, chmurka: Image}}
+ */
+const GRAFIKI = {
+    flappyBird: null,
+    chmurka: null,
+};
+
+// Laduje grafiki i czekam na załadwanie strony
+(async () => await Promise.all([
+    ladujGrafike('grafiki/chmurka.png'),
+    ladujGrafike('grafiki/flappyBird.png'),
+    new Promise(r => window.addEventListener('load', r))
+]).then(([chmurka, flappyBird, ...reszta]) => {
+    Object.assign(GRAFIKI, {chmurka, flappyBird});
+    setup();
+}))();
 
 /**
- * Kontekst kanwasu
+ * @type {HTMLCanvasElement}
+ */
+let canvas;
+
+/**
  * @type {CanvasRenderingContext2D}
  */
 let ctx;
 
+/**
+ * @type {Prostokat}
+ */
+let tloGry;
 
 /**
  * Główna funkcja
@@ -18,11 +43,15 @@ let ctx;
 function setup() {
 
     // Tworze canvas na cały ekran
-    const canvas = document.createElement('canvas');
+    canvas = document.createElement('canvas');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     ctx = canvas.getContext('2d');
     document.body.appendChild(canvas);
+
+    // Tworze tło
+    tloGry = new Prostokat(new Wektor, canvas.width, canvas.height);
+    tloGry.kolor = '#87ceeb';
 
     // Uruchonienie animacji
     animate();
@@ -34,6 +63,10 @@ function setup() {
  */
 function animate() {
 
+    // Rysuje tło
+    tloGry.rysuj(ctx);
+
     // Ponowne załadowanie animacji
     requestAnimationFrame(animate);
+
 }
